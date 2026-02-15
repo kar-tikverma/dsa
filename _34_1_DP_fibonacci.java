a1c38f7cff7e6fee8c3974eba64fa22538ed6851
Kartik Verma <verma.kartik067@gmail.com>
2026-02-15 19:26:35 +0530

    Minor changes

--- _34_1_DP_fibonacci.java
+++ _34_1_DP_fibonacci.java
@@ -1,27 +1,28 @@
 public class _34_1_DP_fibonacci {
-    public static int fibonacci (int n) { // TC -> O(n)
+    public static int fibonacci(int n) { // TC -> O(n)
         if (n < 0) {
             return -1;
         }
 
-        int[] fib = new int[n+1];
+        int[] fib = new int[n + 1];
         fib[1] = 1;
 
         return fibonacci_Util(n, fib);
     }
-    private static int fibonacci_Util (int n, int[] fib) {
+
+    private static int fibonacci_Util(int n, int[] fib) {
         if (n == 0) {
             return 0;
         }
 
         if (fib[n] == 0) {
-            fib[n] = fibonacci_Util(n-1, fib) + fibonacci_Util(n-2, fib);
+            fib[n] = fibonacci_Util(n - 1, fib) + fibonacci_Util(n - 2, fib);
         }
 
         return fib[n];
     }
 
-    public static int climbingStairs_recursion (int n) { // TC -> O(2^n)
+    public static int climbingStairs_recursion(int n) { // TC -> O(2^n)
         if (n < 0) {
             return 0;
         }
@@ -29,15 +30,16 @@ public class _34_1_DP_fibonacci {
             return 1;
         }
 
-        return climbingStairs_recursion(n-1) + climbingStairs_recursion(n-2);
+        return climbingStairs_recursion(n - 1) + climbingStairs_recursion(n - 2);
     }
 
-    public static int climbingStairs_memoization (int n) { // TC -> O(n)
-        int[] ways = new int[n+1];
+    public static int climbingStairs_memoization(int n) { // TC -> O(n)
+        int[] ways = new int[n + 1];
 
         return climbingStairs_memoization_Util(n, ways);
     }
-    private static int climbingStairs_memoization_Util (int n, int[] ways) {
+
+    private static int climbingStairs_memoization_Util(int n, int[] ways) {
         if (n < 0) {
             return 0;
         }
@@ -46,14 +48,14 @@ public class _34_1_DP_fibonacci {
         }
 
         if (ways[n] == 0) {
-            ways[n] = climbingStairs_memoization_Util(n-1, ways)
-                        + climbingStairs_memoization_Util(n-2, ways);
+            ways[n] = climbingStairs_memoization_Util(n - 1, ways)
+                    + climbingStairs_memoization_Util(n - 2, ways);
         }
 
         return ways[n];
     }
 
-    static int climbingStairs_tabulation (int n) { // TC -> O(n)
+    static int climbingStairs_tabulation(int n) { // TC -> O(n)
         if (n < 0) {
             return -1;
         }
@@ -61,18 +63,18 @@ public class _34_1_DP_fibonacci {
             return 1;
         }
 
-        int[] dp = new int[n+1];
+        int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
 
-        for (int i = 2; i < n+1; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        for (int i = 2; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
 
         return dp[n];
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         //
     }
 }
