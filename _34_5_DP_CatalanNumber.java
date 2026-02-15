cd5897e045cb8d3e321a934eb0c559aac6183995
Kartik Verma <verma.kartik067@gmail.com>
2026-02-15 19:22:23 +0530

    Minor changes

--- _34_5_DP_CatalanNumber.java
+++ _34_5_DP_CatalanNumber.java
@@ -1,57 +1,58 @@
 public class _34_5_DP_CatalanNumber {
 
-    public static int catalanNumber_recursion (int n) {
+    public static int catalanNumber_recursion(int n) {
         if (n == 0 || n == 1) {
             return 1;
         }
 
         int res = 0;
         for (int i = 0; i < n; i++) {
-            res += catalanNumber_recursion(i) * catalanNumber_recursion(n-i-1);
+            res += catalanNumber_recursion(i) * catalanNumber_recursion(n - i - 1);
         }
 
         return res;
     }
 
-    public static int catalanNumber_memoization (int n) {
-        int[] dp = new int[n+1];
+    public static int catalanNumber_memoization(int n) {
+        int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
 
         return cat_mem_Util(n, dp);
     }
-    private static int cat_mem_Util (int n, int[] dp) {
+
+    private static int cat_mem_Util(int n, int[] dp) {
         if (dp[n] != 0) {
             return dp[n];
         }
 
         for (int i = 0; i < n; i++) {
-            dp[n] += cat_mem_Util(i, dp) * cat_mem_Util(n-i-1, dp);
+            dp[n] += cat_mem_Util(i, dp) * cat_mem_Util(n - i - 1, dp);
         }
 
         return dp[n];
     }
 
-    public static int catalanNumber_tabulation (int n) { // TC -> O(n ^ 2)
-        int[] dp = new int[n+1];
+    public static int catalanNumber_tabulation(int n) { // TC -> O(n ^ 2)
+        int[] dp = new int[n + 1];
         dp[0] = dp[1] = 1;
 
-        for (int i = 2; i < n+1; i++) {
+        for (int i = 2; i < n + 1; i++) {
             for (int j = 0; j < i; j++) {
-                dp[i] += dp[j] * dp[i-j-1];
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
 
         return dp[n];
     }
 
-    public static int countBST (int nodes) { // TC -> O(n ^ 2)
-        int[] dp = new int[nodes+1];
+    public static int countBST(int nodes) { // TC -> O(n ^ 2)
+        int[] dp = new int[nodes + 1];
         dp[0] = dp[1] = 1;
 
-        for (int i = 2; i < nodes+1; i++) {
+        for (int i = 2; i < nodes + 1; i++) {
             for (int j = 0; j < i; j++) {
                 int left = dp[j];
-                int right = dp[i-j-1];
+                int right = dp[i - j - 1];
                 dp[i] += left * right;
             }
         }
@@ -59,14 +60,14 @@ public class _34_5_DP_CatalanNumber {
         return dp[nodes];
     }
 
-    public static int mountainRanges (int pairs) { // TC -> O(n ^ 2)
-        int[] dp = new int[pairs+1];
+    public static int mountainRanges(int pairs) { // TC -> O(n ^ 2)
+        int[] dp = new int[pairs + 1];
         dp[0] = dp[1] = 1;
 
-        for (int i = 2; i < pairs+1; i++) {
+        for (int i = 2; i < pairs + 1; i++) {
             for (int j = 0; j < i; j++) {
                 int inside = dp[j];
-                int outside = dp[i-j-1];
+                int outside = dp[i - j - 1];
                 dp[i] += inside * outside;
             }
         }
@@ -74,7 +75,7 @@ public class _34_5_DP_CatalanNumber {
         return dp[pairs];
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // int[] arr = {1,2,3,4,5};
         System.out.println();
     }
--- _34_6_DP_Matrices.java
+++ _34_6_DP_Matrices.java
@@ -1,9 +1,10 @@
 public class _34_6_DP_Matrices {
 
-    public static int minCostOfMatrixChainMultiplication_recursion (int[] arr) {
-        return mcm_rec_Util(1, arr.length-1, arr);
+    public static int minCostOfMatrixChainMultiplication_recursion(int[] arr) {
+        return mcm_rec_Util(1, arr.length - 1, arr);
     }
-    private static int mcm_rec_Util (int i, int j, int[] arr) {
+
+    private static int mcm_rec_Util(int i, int j, int[] arr) {
         // Same matrix case
         if (i == j) {
             return 0;
@@ -13,21 +14,22 @@ public class _34_6_DP_Matrices {
 
         for (int k = i; k < j; k++) {
             int firstPart = mcm_rec_Util(i, k, arr);
-            int secondPart = mcm_rec_Util(k+1, j, arr);
+            int secondPart = mcm_rec_Util(k + 1, j, arr);
 
-            int cost = arr[i-1] * arr[k] * arr[j];
-            minCost = Math.min (minCost, firstPart + secondPart + cost);
+            int cost = arr[i - 1] * arr[k] * arr[j];
+            minCost = Math.min(minCost, firstPart + secondPart + cost);
         }
 
         return minCost;
     }
 
-    public static int minCostOfMatrixChainMultiplication_memoization (int[] arr) { // TC -> O(n^2)
+    public static int minCostOfMatrixChainMultiplication_memoization(int[] arr) { // TC -> O(n^2)
         int[][] dp = new int[arr.length][arr.length];
 
-        return mcm_mem_Util(1, arr.length-1, arr, dp);
+        return mcm_mem_Util(1, arr.length - 1, arr, dp);
     }
-    private static int mcm_mem_Util (int i, int j, int[] arr, int[][] dp) {
+
+    private static int mcm_mem_Util(int i, int j, int[] arr, int[][] dp) {
         // Same matrix case
         if (i == j) {
             return 0;
@@ -40,38 +42,38 @@ public class _34_6_DP_Matrices {
 
         for (int k = i; k < j; k++) {
             int firstPart = mcm_mem_Util(i, k, arr, dp);
-            int secondPart = mcm_mem_Util(k+1, j, arr, dp);
+            int secondPart = mcm_mem_Util(k + 1, j, arr, dp);
 
-            int cost = arr[i-1] * arr[k] * arr[j];
-            minCost = Math.min (minCost, firstPart + secondPart + cost);
+            int cost = arr[i - 1] * arr[k] * arr[j];
+            minCost = Math.min(minCost, firstPart + secondPart + cost);
         }
 
         return dp[i][j] = minCost;
     }
 
-    public static int minCostOfMatrixChainMultiplication_tabulation (int[] arr) { // TC -> O(n^2)
+    public static int minCostOfMatrixChainMultiplication_tabulation(int[] arr) { // TC -> O(n^2)
         int n = arr.length;
         int[][] dp = new int[n][n];
 
         // Initialization steps include assigning dp[i][i] = 0
-        
+
         for (int len = 2; len < n; len++) {
-            for (int i = 1, j = len; i <= n-len; i++, j++) {
+            for (int i = 1, j = len; i <= n - len; i++, j++) {
                 dp[i][j] = Integer.MAX_VALUE;
                 for (int k = i; k < j; k++) {
                     int firstPart = dp[i][k];
-                    int secondPart = dp[k+1][j];
-                    int cost = arr[i-1] * arr[k] * arr[j];
+                    int secondPart = dp[k + 1][j];
+                    int cost = arr[i - 1] * arr[k] * arr[j];
                     dp[i][j] = Math.min(dp[i][j], firstPart + secondPart + cost);
                 }
             }
         }
 
-        return dp[1][n-1];
+        return dp[1][n - 1];
     }
 
-    public static void main (String[] args) {
-        int[] arr = {1,2,3,4,3};
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 3 };
         System.out.println(minCostOfMatrixChainMultiplication_tabulation(arr));
     }
 
