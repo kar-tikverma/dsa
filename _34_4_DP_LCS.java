3fb2895123aca6653ff941e25d834362855e3754
Kartik Verma <verma.kartik067@gmail.com>
2026-02-15 19:21:19 +0530

    Minor changes

--- _34_4_DP_LCS.java
+++ _34_4_DP_LCS.java
@@ -2,33 +2,35 @@ import java.util.*;
 
 public class _34_4_DP_LCS {
 
-    public static int longestCommonSubsequence_recursion (String str1, String str2) {
+    public static int longestCommonSubsequence_recursion(String str1, String str2) {
         return LCS_recursion_Util(str1, str2, str1.length(), str2.length());
     }
-    private static int LCS_recursion_Util (String str1, String str2, int len1, int len2) {
+
+    private static int LCS_recursion_Util(String str1, String str2, int len1, int len2) {
         if (len1 == 0 || len2 == 0) {
             return 0;
         }
 
-        if (str1.charAt(len1-1) == str2.charAt(len2-1)) {
+        if (str1.charAt(len1 - 1) == str2.charAt(len2 - 1)) {
             return 1 + LCS_recursion_Util(str1, str2, len1 - 1, len2 - 1);
         }
-        
+
         int res1 = LCS_recursion_Util(str1, str2, len1, len2 - 1);
         int res2 = LCS_recursion_Util(str1, str2, len1 - 1, len2);
 
         return Math.max(res1, res2);
     }
 
-    public static int longestCommonSubsequence_memoization (String str1, String str2) {
-        int[][] dp = new int[str1.length()+1][str2.length()+1];
+    public static int longestCommonSubsequence_memoization(String str1, String str2) {
+        int[][] dp = new int[str1.length() + 1][str2.length() + 1];
         for (int i = 0; i < dp.length; i++) {
             Arrays.fill(dp[i], -1);
         }
-        
+
         return LCS_memoization_Util(str1, str2, str1.length(), str2.length(), dp);
     }
-    private static int LCS_memoization_Util (String str1, String str2, int len1, int len2, int[][] dp) {
+
+    private static int LCS_memoization_Util(String str1, String str2, int len1, int len2, int[][] dp) {
         if (len1 == 0 || len2 == 0) {
             return 0;
         }
@@ -37,27 +39,27 @@ public class _34_4_DP_LCS {
             return dp[len1][len2];
         }
 
-        if (str1.charAt(len1-1) == str2.charAt(len2-1)) {
+        if (str1.charAt(len1 - 1) == str2.charAt(len2 - 1)) {
             return dp[len1][len2] = 1 + LCS_memoization_Util(str1, str2, len1 - 1, len2 - 1, dp);
         }
-        
+
         int res1 = LCS_memoization_Util(str1, str2, len1, len2 - 1, dp);
         int res2 = LCS_memoization_Util(str1, str2, len1 - 1, len2, dp);
 
         return dp[len1][len2] = Math.max(res1, res2);
     }
 
-    public static int longestCommonSubsequence_tabulation (String str1, String str2) {
+    public static int longestCommonSubsequence_tabulation(String str1, String str2) {
         int n = str1.length();
         int m = str2.length();
-        int[][] dp = new int[n+1][m+1];
-        
+        int[][] dp = new int[n + 1][m + 1];
+
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
-                if (str1.charAt(i-1) == str2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
@@ -65,17 +67,17 @@ public class _34_4_DP_LCS {
         return dp[n][m];
     }
 
-    public static int longestCommonSubstring_tabulation (String str1, String str2) {
+    public static int longestCommonSubstring_tabulation(String str1, String str2) {
         int n = str1.length();
         int m = str2.length();
-        int[][] dp = new int[n+1][m+1];
-        
+        int[][] dp = new int[n + 1][m + 1];
+
         int ans = 0;
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
-                if (str1.charAt(i-1) == str2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                     ans = Math.max(ans, dp[i][j]);
                 } else {
                     dp[i][j] = 0;
@@ -86,7 +88,7 @@ public class _34_4_DP_LCS {
         return ans;
     }
 
-    public static int longestIncreasingSubsequence (int[] arr) {
+    public static int longestIncreasingSubsequence(int[] arr) {
         HashSet<Integer> s = new HashSet<>();
         for (int i = 0; i < arr.length; i++) {
             s.add(arr[i]);
@@ -99,19 +101,20 @@ public class _34_4_DP_LCS {
         }
         Arrays.sort(temp);
 
-        return LCS (arr, temp);
+        return LCS(arr, temp);
     }
-    private static int LCS (int[] arr1, int[] arr2) {
+
+    private static int LCS(int[] arr1, int[] arr2) {
         int n = arr1.length;
         int m = arr2.length;
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[n + 1][m + 1];
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
-                if (arr1[i-1] == arr2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
+                if (arr1[i - 1] == arr2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
@@ -121,11 +124,11 @@ public class _34_4_DP_LCS {
 
     // Leetcode 72
     // Runtime = 5 ms (Beats 68.11%)
-    public static int editDistance (String word1, String word2) {
+    public static int editDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
 
-        int[][] dp = new int[n+1][m+1];
+        int[][] dp = new int[n + 1][m + 1];
         for (int i = 1; i < m + 1; i++) {
             dp[0][i] = i;
         }
@@ -135,13 +138,13 @@ public class _34_4_DP_LCS {
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
-                if (word1.charAt(i-1) == word2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1];
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else {
-                    int add = dp[i][j-1] + 1;
-                    int del = dp[i-1][j] + 1;
-                    int rep = dp[i-1][j-1] + 1;
-                    dp[i][j] = Math.min (add, Math.min(del, rep));
+                    int add = dp[i][j - 1] + 1;
+                    int del = dp[i - 1][j] + 1;
+                    int rep = dp[i - 1][j - 1] + 1;
+                    dp[i][j] = Math.min(add, Math.min(del, rep));
                 }
             }
         }
@@ -149,25 +152,25 @@ public class _34_4_DP_LCS {
         return dp[n][m];
     }
 
-    public static int stringConversion (String word1, String word2) {
-        // Different from editDistance() as in this only insertion and deletion is allowed
-        // and replacement is not.
+    public static int stringConversion(String word1, String word2) {
+        // Different from editDistance() as in this only insertion and deletion is
+        // allowed and replacement is not.
         int LCS = longestCommonSubsequence_tabulation(word1, word2);
         return word1.length() - LCS + word2.length() - LCS;
     }
 
     // Leetcode #44
     // Solution 1: Runtime = 19 ms (Beats 69.6%)
-    public static boolean wildcardMatching_1 (String str, String pattern) { // TC -> O(n * m)
+    public static boolean wildcardMatching_1(String str, String pattern) { // TC -> O(n * m)
         int n = str.length();
         int m = pattern.length();
-        boolean[][] dp = new boolean[n+1][m+1];
+        boolean[][] dp = new boolean[n + 1][m + 1];
 
         // Initialization
         dp[0][0] = true;
         // Initialize dp[i][0] = false
         for (int j = 1; j < m + 1; j++) {
-            if (pattern.charAt(j-1) == '*') {
+            if (pattern.charAt(j - 1) == '*') {
                 dp[0][j] = true;
             } else {
                 break;
@@ -176,11 +179,11 @@ public class _34_4_DP_LCS {
 
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j < m + 1; j++) {
-                char ch = pattern.charAt(j-1);
-                if (ch == '?' || ch == str.charAt(i-1)) {
-                    dp[i][j] = dp[i-1][j-1];
+                char ch = pattern.charAt(j - 1);
+                if (ch == '?' || ch == str.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
                 } else if (ch == '*') {
-                    if (dp[i-1][j] || dp[i][j-1]) {
+                    if (dp[i - 1][j] || dp[i][j - 1]) {
                         dp[i][j] = true;
                     }
                 }
@@ -191,7 +194,7 @@ public class _34_4_DP_LCS {
     }
 
     // Solution 2: Runtime = 1 ms
-    public static boolean wildcardMatching (String str, String pattern) {
+    public static boolean wildcardMatching(String str, String pattern) {
         int n = str.length();
         int m = pattern.length();
 
@@ -201,7 +204,7 @@ public class _34_4_DP_LCS {
         int starAt = -1;
         int matchingTill = -1;
 
-        while(si < n) {
+        while (si < n) {
             if (pi < m && (pattern.charAt(pi) == '?' || pattern.charAt(pi) == str.charAt(si))) {
                 si++;
                 pi++;
@@ -218,13 +221,13 @@ public class _34_4_DP_LCS {
             }
         }
 
-        while (pi < m && pattern.charAt(pi) == '*') 
+        while (pi < m && pattern.charAt(pi) == '*')
             pi++;
-        
+
         return pi == m;
     }
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         // int[] arr = {50, 3, 10, 7, 40, 80};
         System.out.println(wildcardMatching("abc", "**?b*"));
     }
