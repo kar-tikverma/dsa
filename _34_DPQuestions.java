96c92d3b32dc10a3d7081b37417e4dea96898987
Kartik Verma <verma.kartik067@gmail.com>
2026-03-09 22:30:55 +0530

    added Leetcode_3129

--- _34_DPQuestions.java
+++ _34_DPQuestions.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class _34_DP {
+public class _34_DPQuestions {
     // Leetcode #2035
     // We can solve it using 0-1 knapsack only if negative numbers are not allowed.
     // Solution 1: Runtime = 394 ms (Beats 93.9%)
@@ -219,6 +219,47 @@ public class _34_DP {
         }
 
         return maxSum == Long.MIN_VALUE ? 0 : maxSum;
+    }
+
+    class Leetcode_3129 {
+        public int numberOfStableArrays(int zero, int one, int limit) {
+            final long MOD = 1000000007;
+            long[][][] dp = new long[zero + 1][one + 1][2];
+
+            for (int i = 0; i <= Math.min(zero, limit); i++) {
+                dp[i][0][0] = 1;
+            }
+
+            for (int j = 0; j <= Math.min(one, limit); j++) {
+                dp[0][j][1] = 1;
+            }
+
+            for (int i = 1; i <= zero; i++) {
+                for (int j = 1; j <= one; j++) {
+                    if (i > limit) {
+                        dp[i][j][0] = dp[i - 1][j][0]
+                                + dp[i - 1][j][1]
+                                - dp[i - limit - 1][j][1];
+                    } else {
+                        dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                    }
+
+                    dp[i][j][0] = ((dp[i][j][0] % MOD) + MOD) % MOD;
+
+                    if (j > limit) {
+                        dp[i][j][1] = dp[i][j - 1][1]
+                                + dp[i][j - 1][0]
+                                - dp[i][j - limit - 1][0];
+                    } else {
+                        dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
+                    }
+
+                    dp[i][j][1] = ((dp[i][j][1] % MOD) + MOD) % MOD;
+                }
+            }
+
+            return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+        }
     }
 
     public static void main(String[] args) {
